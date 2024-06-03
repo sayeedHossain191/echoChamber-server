609cb355ab14529738111427e2b70bd4fbb2e90c
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-06-03 22:06:56 +0600

    fifth commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.bbzq5pl.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
 
@@ -30,6 +31,33 @@ async function run() {
         const userCollection = client.db("Forum").collection("users");
         const postCollection = client.db("Forum").collection("posts");
 
+
+
+        //jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' });
+            res.send({ token })
+        })
+
+
+        //middlewares
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'forbidden access' })
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next()
+            })
+        }
+
+
+
         //Post related api
         app.get('/posts', async (req, res) => {
             const filter = req.query;
@@ -45,11 +73,27 @@ async function run() {
             res.send(result)
         })
 
+
+
         //Users related api
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
             console.log(req.headers)
             const result = await userCollection.find().toArray();
             res.send(result)
+        })
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'Unauthorized Access' })
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin });
         })
 
         app.post('/users', async (req, res) => {
@@ -73,9 +117,11 @@ async function run() {
                     role: 'admin'
                 }
             }
-            const result = await userCollection.updatedOne(filter, updatedDoc)
+            const result = await userCollection.updateOne(filter, updatedDoc)
             res.send(result)
         })
+
+
 
 
 
