7d042eb09668527ef7005bda44a2642d641026a1
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-06-06 13:55:34 +0600

    new commit

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
 
+
 //middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5177",
+            "https://b9a12-forum-server.vercel.app",
+            "https://b9a12-forum-client.web.app",
+        ],
+        credentials: true,
+        optionSuccessStatus: 200,
+    })
+);
 app.use(express.json());
 
 
@@ -27,7 +39,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
 
         const userCollection = client.db("Forum").collection("users");
         const postCollection = client.db("Forum").collection("posts");
@@ -35,37 +47,40 @@ async function run() {
 
 
 
-        //jwt related api
+        // jwt related api
         app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' });
-            res.send({ token })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
         })
 
 
-        //middlewares
+
+
+        // middlewares 
         const verifyToken = (req, res, next) => {
+            // console.log('inside verify token', req.headers.authorization);
             if (!req.headers.authorization) {
-                return res.status(401).send({ message: 'Unauthorized Access' })
+                return res.status(401).send({ message: 'unauthorized access' });
             }
-            const token = req.headers.authorization.split(' ')[1]
+            const token = req.headers.authorization.split(' ')[1];
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
                 if (err) {
-                    return res.status(401).send({ message: 'Unauthorized Access' })
+                    return res.status(401).send({ message: 'unauthorized access' })
                 }
                 req.decoded = decoded;
-                next()
+                next();
             })
         }
 
-        //Use verify admin after verifyToken
+        // use verify admin after verifyToken
         const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email;
-            const query = { email: email }
+            const query = { email: email };
             const user = await userCollection.findOne(query);
             const isAdmin = user?.role === 'admin';
-            if (isAdmin) {
-                return res.status(403).send({ message: 'Forbidden Access' });
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
             }
             next();
         }
@@ -88,6 +103,26 @@ async function run() {
         })
 
 
+        //Pagination starts
+        app.get('/all-posts', async (req, res) => {
+            const page = parseInt(req.query.page) - 1
+            const size = parseInt(req.query.size)
+
+            const cursor = postCollection.find();
+            const result = await cursor
+                .skip(page * size)
+                .limit(size)
+                .toArray();
+            res.send(result)
+        })
+        //Post count
+        app.get('/post-count', async (req, res) => {
+            const count = await postCollection.estimatedDocumentCount();
+            res.send({ count })
+        })
+        //Pagination Ends
+
+
 
         //Users related api
         app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
@@ -98,41 +133,43 @@ async function run() {
 
         app.get('/users/admin/:email', verifyToken, async (req, res) => {
             const email = req.params.email;
+
             if (email !== req.decoded.email) {
-                return res.status(403).send({ message: 'Forbidden Access' })
+                return res.status(403).send({ message: 'forbidden access' })
             }
+
             const query = { email: email };
             const user = await userCollection.findOne(query);
             let admin = false;
             if (user) {
-                admin = user?.role === 'admin'
+                admin = user?.role === 'admin';
             }
             res.send({ admin });
         })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
-
-            //insert email if user doesn't exist
+            // insert email if user doesnt exists: 
+            // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
             const query = { email: user.email }
-            const existingUser = await userCollection.findOne(query)
+            const existingUser = await userCollection.findOne(query);
             if (existingUser) {
-                return res.send({ message: 'user already exist', insertedId: null })
+                return res.send({ message: 'user already exists', insertedId: null })
             }
             const result = await userCollection.insertOne(user);
-            res.send(result)
-        })
+            res.send(result);
+        });
 
         app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
+            const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
                 $set: {
                     role: 'admin'
                 }
             }
-            const result = await userCollection.updateOne(filter, updatedDoc)
-            res.send(result)
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
         })
 
 
@@ -147,10 +184,11 @@ async function run() {
 
 
 
-        //Payment Intent
+        // payment intent
         app.post('/create-payment-intent', async (req, res) => {
             const { price } = req.body;
             const amount = parseInt(price * 100);
+            console.log(amount, 'amount inside the intent')
 
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
@@ -161,7 +199,7 @@ async function run() {
             res.send({
                 clientSecret: paymentIntent.client_secret
             })
-        })
+        });
 
 
         // stats or analytics
@@ -194,7 +232,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
@@ -204,9 +242,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Forum is created')
+    res.send('Forum is running')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Forum is running on port ${port}`)
 })
